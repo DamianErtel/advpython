32678f78a5843424875d6359756c20a1c5174aef
rancid97 <damian.ertel5@gmail.com>
2022-06-30 13:54:25 +0200

    add descriptions

--- html_builder.py
+++ html_builder.py
@@ -7,6 +7,7 @@ html_boilerplate_middle = '<body>'
 html_boilerplate_bottom = '</body></html>'
 
 python_v = platform.python_version()
+python_interpreter_name = platform.python_version()
 python_interpreter = sys.version
 system_type = platform.system()
 system_v = platform.release()
@@ -17,7 +18,8 @@ sys_info_base = '''
 <h2>Execution environment</h2>
 <p>
 Python version: python_v<br/>
-Interpreter: python_interpreter<br/>
+Interpreter: python_interpreter_name<br/>
+Interprete versionr: python_interpreter<br/>
 Operating system: system_type<br/>
 Operating system version: system_v<br/>
 Processor: processor_type<br/>
@@ -27,6 +29,7 @@ CPUs: processor_count
 
 sys_info_complete = (sys_info_base
                      .replace("python_v", str(python_v))
+                     .replace("python_interpreter_name", str(python_interpreter_name))
                      .replace("python_interpreter", str(python_interpreter))
                      .replace("system_type", str(system_type))
                      .replace("system_v", str(system_v))
@@ -34,8 +37,12 @@ sys_info_complete = (sys_info_base
                      .replace("processor_count", str(processor_count))
                      )
 
+project_header = '<h1>Multithreading/Multiprocessing benchmark results</h1>'
 execution = '<div><h2>Execution:</h2><span><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div></span></div>'
 execution_median = '<div><h2>Execution:</h2><span>Median:</span></div>'
+top_heading = '<div><h1>Test results</h1><p>The following table shows detailed test results:</p></div>'
+bottom_heading = '<div><h1>Summary</h1><p>The following table shows the median of all results:</p></div>'
+author = '<span>App author: Damian Ertel</span>'
 
 def createColumn(results, tag):
     table_data = ""
@@ -83,7 +90,7 @@ def build_html(results, median_list, nameplate_list):
 
     median_section += '</section>'
 
-    ready_html = html_boilerplate_top + html_boilerplate_middle + sys_info_complete + cols + median_section + html_boilerplate_bottom
+    ready_html = html_boilerplate_top + html_boilerplate_middle + project_header + sys_info_complete + top_heading + cols + bottom_heading + median_section + author + html_boilerplate_bottom
 
     f = open("./results/myfile.html", "w")
     f.write(ready_html)
